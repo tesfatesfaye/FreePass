50e5f993575febb5d1fd3a2e2414eaf2d0de6443
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-11-22 15:34:17 -0500

    cleaned up react components and optimized css file

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav>
+    
+  </nav>;
+};
+export default Navbar;
--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,27 +1,11 @@
-// import { useAuth0 } from "@auth0/auth0-react";
 import { FC } from "react";
 import useProfile from "../hooks/useProfile.tsx"
-interface ProfileProps {
-  name: string;
-  email: string;
-  picture?: string; 
-}
 const Profile: FC = () => {
-  // const { user , isAuthenticated, isLoading } = useAuth0<ProfileProps>();
-  const { user } = useProfile("1f605d6c-97d0-4281-885b-87d420eb3ddc");
-  console.log("data",user?.email)
-  // if (isLoading) {
-  //   return <div>Loading ...</div>;
-  // }
+ const { user } = useProfile("1f605d6c-97d0-4281-885b-87d420eb3ddc");
+ console.log(user);
   return (
     <>
-      {/* {data?.user?.email} */}
-
-      {/* {isAuthenticated && user && ( */}
-        <div>
-          <div>{user?.email} </div>
-                 </div>
-      {/* )} */}
+     
     </>
   );
 };
