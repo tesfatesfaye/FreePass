0f073f1cc4cdb52cfd989aa1518d828f95704e70
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-11-29 15:41:04 -0500

    renamed arg

--- client/src/utils/componentMappers.tsx
+++ client/src/utils/componentMappers.tsx
@@ -1,13 +1,13 @@
 import type { SideBarItem } from "../types/reactTypes/sideBarType";
 // functions used for mapping react components
-const sideBarMapper = (items:SideBarItem[], height:boolean = true) => {
+const sideBarMapper = (items:SideBarItem[], isOpen:boolean = true) => {
   return items.map((item) => {
 
     return (
       <div
         key={item.id}
         className={`flex text-white items-center gap-2 font-semibold ml-2 cursor-pointer mr-auto overflow-hidden transition-all duration-300
-        ${height ? " mb-3 h-auto" : "m-0 p-0 h-0"}`}
+        ${isOpen ? " mb-3 h-auto" : "m-0 p-0 h-0"}`}
       >
         <item.icon />
         {item.name}
