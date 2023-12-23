069dee51aa6533450ba70830e35a1b61c7c8d1c5
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-22 21:07:41 -0500

    renamed the import

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,18 +1,16 @@
 import { FC } from "react";
-import { TbFingerprintScan } from "react-icons/tb";
+import MainIcon from "../icons/IconComponent";
 import Avatar from "./Avatar";
-import IconComponent from "../icons/IconComponent";
 import SearchBar from "./SearchBar";
 const Navbar: FC = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap p-2 py-2 bg-p-dark border-b border-b-t-dark">
       <button>
-        <IconComponent
+        <MainIcon
           type="MainIcon"
           className="cursor-pointer text-white"
           size={30}
         />
-       
       </button>
       <SearchBar />
       <Avatar />
