48ab929b097f489d047d9a63006445c51f0539cc
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-22 20:57:06 -0500

    updated the css of said component

--- client/src/components/secrets/selectedSecret/SelectedSecret.tsx
+++ client/src/components/secrets/selectedSecret/SelectedSecret.tsx
@@ -2,10 +2,10 @@ import React,{FC} from "react";
 
 const SelectedSecret:FC = () => {
     return (
-        <div className="text-white flex w-full h-full items-center justify-center">
-            <h1>Selected Secret</h1>       
-        </div>
-    )
+      <div className="text-white flex w-[1010px] h-full flex-grow  items-center justify-center">
+        <h1>Selected Secret</h1>
+      </div>
+    );
 }
 
 export default SelectedSecret
