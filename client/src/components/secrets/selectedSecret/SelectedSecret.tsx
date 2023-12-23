c9e851dd000fbc48c23822fcfd7f4acbb5879c1d
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-23 01:18:08 -0500

    moved ssn component to appropriate directory

--- client/src/components/secrets/selectedSecret/SelectedSecret.tsx
+++ client/src/components/secrets/selectedSecret/SelectedSecret.tsx
@@ -3,7 +3,7 @@ import IconComponent from "../../icons/IconComponent";
 const SelectedSecret:FC = () => {
     return (
       <div className="text-white flex h-full flex-grow  items-center justify-center opacity-60">
-        <IconComponent type="MainIcon" size={150} />
+        <IconComponent type="MainIcon" size={175} />
       </div>
     );
 }
