18b85eb2ae02cab0c7bb7b96a55c9a2bc9d6f727
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-24 15:07:38 -0500

    compartmentalized modals

--- client/src/modals/AddSecret.tsx
+++ client/src/modals/AddSecret.tsx
@@ -1,17 +1,15 @@
 import { FC, useContext } from "react";
 import { DashboardContext } from "../context/DashboardContext";
-import ReactPortal from "./ReactPortal";
-
+import Modal from "./Modal";
 const AddSecret: FC = () => {
   const { currentModal } = useContext(DashboardContext);
   if (currentModal !== "AddSecret") return null;
   return (
-    <ReactPortal wrapperId="portal">
-      <div className="modal-overlay"></div>
-      <div className="flex fixed top-0 left-0 right-0 bottom-0 z-50 text-white">
-       
+    <Modal>
+      <div className="modal-container justify-center">
+        <div>Hello</div>
       </div>
-    </ReactPortal>
+    </Modal>
   );
 };
 export default AddSecret;
--- client/src/modals/Modal.tsx
+++ client/src/modals/Modal.tsx
@@ -1,4 +1,4 @@
-import React,{FC,ReactNode} from "react";
+import {FC,ReactNode} from "react";
 import ReactPortal from "./ReactPortal";
 interface ModalProps {
     children: ReactNode;
