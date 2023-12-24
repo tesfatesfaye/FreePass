945cdd2055f9dd0f0cafe068c1365fe76086ccf6
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-24 15:03:01 -0500

    added new modal componenet

---
+++ client/src/modals/Modal.tsx
@@ -0,0 +1,20 @@
+import React,{FC,ReactNode} from "react";
+import ReactPortal from "./ReactPortal";
+interface ModalProps {
+    children: ReactNode;
+    
+}
+
+const Modal: FC<ModalProps> = ({ children }) => {
+  return (
+    <ReactPortal wrapperId="portal">
+      <div className="modal-overlay"></div>
+
+      {children}
+    </ReactPortal>
+  );
+};
+
+
+     
+export default Modal;
