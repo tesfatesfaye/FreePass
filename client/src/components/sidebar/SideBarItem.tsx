f6852e3807e2bf37447059a392efaaa8e2dd2ba1
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-24 11:51:56 -0500

    made changes to the sidebarItem componenet

--- client/src/components/sidebar/SideBarItem.tsx
+++ client/src/components/sidebar/SideBarItem.tsx
@@ -3,7 +3,6 @@ import { DashboardContext } from "../../context/DashboardContext";
 import type { SideBarItemType } from "../../types/reactTypes/sideBarType";
 import IconComponent from "../icons/IconComponent";
 
-
 const SideBarItem: FC<SideBarItemType> = (props) => {
   const { sideBarItemIsSelected, toggleSideBarItemSelection } =
     useContext(DashboardContext);
@@ -13,11 +12,11 @@ const SideBarItem: FC<SideBarItemType> = (props) => {
       key={props.id}
       onClick={() => toggleSideBarItemSelection(props.name)}
       className={`flex text-white items-center gap-2  pl-2 pt-0 pb-0 cursor-pointer 
-          pr-auto overflow-hidden transition-all duration-300 text-lg
+          pr-auto overflow-hidden transition-all duration-200 text-lg
            ${
              props.isOpen
-               ? " mb-3 h-auto"
-               : "m-0 p-0 pt-[0.05rem] pb-[0.05rem] h-0 "
+               ? " mb-2 max-h-[3rem] pt-[0.05rem] pb-[0.05rem]"
+               : "m-0 p-0  max-h-0 "
            } ${
         props.name === sideBarItemIsSelected
           ? " bg-white bg-opacity-20"
