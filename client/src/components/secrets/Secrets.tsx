1212c27fa45daa60450eea34e33f0ae1cef0717e
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-24 11:49:30 -0500

    made changes to the secrets componenet

--- client/src/components/secrets/Secrets.tsx
+++ client/src/components/secrets/Secrets.tsx
@@ -4,12 +4,12 @@ import SecretItem from "./SecretItem";
 const SecretParent: FC = () => {
   const { secretParentHeight } = useContext(DashboardContext);
   const secrets = Array.from({ length: 10 }, (j, i) => (
-    <SecretItem key={String(i)} id={String(i)} subType="Email"type="Key" />
+    <SecretItem key={String(i)} id={String(i)} subtype="Email"type="Key" />
   ));
   return (
     <div
       style={{ maxHeight: secretParentHeight }}
-      className={`flex flex-col w-[400px] px] bg-p-dark text-white  border-l border-l-t-dark  overflow-y-auto  `}
+      className={`flex flex-col w-[22%] px] bg-p-dark text-white  border-l border-l-t-dark  overflow-y-auto  `}
     >
       {secrets}
     </div>
--- client/src/components/secrets/selectedSecret/SelectedSecret.tsx
+++ client/src/components/secrets/selectedSecret/SelectedSecret.tsx
@@ -1,8 +1,18 @@
-import React,{FC} from "react"; 
+import React,{FC,useEffect,useRef} from "react"; 
 import IconComponent from "../../icons/IconComponent";
 const SelectedSecret:FC = () => {
+const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const handleResize = () => {
+      if(ref.current!==null){
+           console.log(ref.current.offsetWidth)}
+    };
+    handleResize()
+  
+  },)
+
     return (
-      <div className="text-white flex h-full flex-grow  items-center justify-center opacity-60">
+      <div ref={ref}className="text-white flex h-full flex-grow  items-center justify-center opacity-60">
         <IconComponent type="MainIcon" size={175} />
       </div>
     );
