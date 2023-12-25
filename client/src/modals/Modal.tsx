ddcb60b190ca2ca86b9577803a12e74148c99948
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-24 19:39:04 -0500

    removed unused code for the modal tsx file

--- client/src/modals/Modal.tsx
+++ client/src/modals/Modal.tsx
@@ -1,14 +1,12 @@
 import {FC,ReactNode,useContext} from "react";
 import ReactPortal from "./ReactPortal";
-import { DashboardContext } from "../context/DashboardContext";
-import IconComponent from "../components/icons/IconComponent";
 interface ModalProps {
     children: ReactNode;
     
 }
 
 const Modal: FC<ModalProps> = ({ children }) => {
-  const {updateCurrentModal}=useContext(DashboardContext)
+
     return (
       <ReactPortal wrapperId="portal">
         <div className="modal-overlay">
