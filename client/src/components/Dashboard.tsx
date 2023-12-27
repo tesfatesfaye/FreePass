7e9ab89a7e51494aa522c6f9e49916b211007763
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-27 17:08:30 -0500

    changed the name of the Login component

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import SecretParent from "./secrets/Secrets.tsx";
 import SideBar from "./sidebar/SideBar.tsx";
 import SelectedSecret from "./secrets/selectedSecret/SelectedSecret.tsx";
-import LoginSecretComponent from "./secrets/selectedSecret/LoginSecretComponent.tsx";
+import LoginSecretComponent from "./secrets/selectedSecret/Login.tsx";
 const DashBoard: FC = () => {
   return (
     <div className="flex h-full w-full">
