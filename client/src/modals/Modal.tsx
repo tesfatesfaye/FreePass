e07d765de85f7c533af41f4714b11710599e9625
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-27 17:21:52 -0500

    Modified the modal and using it as a test bed.

--- client/src/modals/Modal.tsx
+++ client/src/modals/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode, useContext } from "react";
 import { DashboardContext } from "../context/DashboardContext";
 import ReactPortal from "./ReactPortal";
+import Icon from "../components/icons/IconComponent";
 interface ModalProps {
   children: ReactNode;
 }
@@ -12,23 +13,25 @@ const Modal: FC<ModalProps> = ({ children }) => {
       <div className="modal-container">
         <div
           className="flex flex-col relative
-        h-[95%]   bg-p-dark rounded-md shadow-2xl w-[30%]  "
+        h-[95%] bg-p-dark rounded-md shadow-2xl w-[30%] max-w-[700px] "
         >
-          <header className="flex items-center h-[4rem] justify-between px-4 rounded-t-md bg-s-dark ">
-            <h1 className="text-white text-lg  ml-auto mr-auto">Add Secret</h1>
-            <button
-              onClick={() => updateCurrentModal(null)}
-              className="text-white flex items-center text-lg font-bold mr-2"
-            >
-              X
+          <header className="flex items-center h-[4rem] justify-between px-4 rounded-t-md bg-s-dark">
+            <Icon type="Login" size={45}className="text-white text-lg  ml-auto mr-auto"/>
+            <button onClick={() => updateCurrentModal(null)}>
+              <Icon
+                className="text-white flex items-center text-lg font-bold mr-2"
+                type="Close"
+                size={25}
+              />
             </button>
           </header>
           {children}
-          <footer className="flex items-center h-[3rem] mt-auto justify-between px-4 rounded-b-md bg-s-dark">
+          <footer className="flex items-center h-[3rem] mt-auto justify-between px-4 
+          rounded-b-md bg-s-dark">
             <button
-              className="text-slate-300
-               flex w-[5rem] items-center justify-center h-[2rem
-            ] bg-p-dark ml-auto mr-2 rounded"
+              className="text-white 
+               flex w-[5rem] items-center justify-center h-[30px]
+             bg-p-dark ml-auto mr-2 rounded"
             >
               Save
             </button>
