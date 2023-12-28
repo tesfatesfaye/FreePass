a58746db5610562694e5dfee3c1a84dc820ea161
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:27:11 -0500

    Add SidebarDataType interface

---
+++ client/src/types/reactTypes/SidebarDataType.ts
@@ -0,0 +1,10 @@
+import { IconNames } from "./iconNamesTypes";
+interface SideBarDataType {
+  id: string;
+  name: string;
+  icon: IconNames;
+  color?: string;
+
+}
+
+export type { SideBarDataType };
