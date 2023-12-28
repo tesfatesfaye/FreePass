751c963cd4b05322b2ab9a9de7fe8563f25b8096
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-27 19:36:33 -0500

    Add SSOToggle component to useLogin hook

--- client/src/hooks/useLogin.tsx
+++ client/src/hooks/useLogin.tsx
@@ -1,6 +1,7 @@
-import {useState} from 'react';
+import {useState,FC} from 'react';
 import ModalInput from '../modals/UIcomponents/ModalInput';
 import { v4 } from 'uuid';
+import ToggleElement from '../modals/UIcomponents/ToggleElement';
 const useLogin = () => {   
 
     const [sso,setSso] = useState<boolean>(false);
@@ -17,12 +18,16 @@ const useLogin = () => {
       { id: v4(), type: "url", placeholder: "http://google.com", name: "url" },
     ];
 
+    const SSOToggle :FC= () => (
+      <ToggleElement value={sso} name="SSO"  toggler={toggleSso} />
+    );
+
     const loginInputsList=loginInputs.map((input)=>{
         return <ModalInput key={input.id} type={input.type} placeholder={input.placeholder} name={input.name} />
     
     })
 
-    return {sso,toggleSso,loginInputsList}
+    return { sso, toggleSso, loginInputsList, SSOToggle };
 
 }
 
