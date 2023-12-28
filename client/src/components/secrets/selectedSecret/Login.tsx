a3d4c56d88133a9fea25eea1e81a60a21e9b8069
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:25:52 -0500

    Add TextArea component to Login.tsx

--- client/src/components/secrets/selectedSecret/Login.tsx
+++ client/src/components/secrets/selectedSecret/Login.tsx
@@ -3,10 +3,11 @@ import { DashboardContext } from "../../../context/DashboardContext";
 import useLogin from "../../../hooks/useLogin";
 import Modal from "../../../modals/Modal";
 import TitleContainer from "../../../modals/UIcomponents/TitleContainer";
+import TextArea from "../../../modals/UIcomponents/TextArea";
 import AddTags from "../../../modals/UIcomponents/AddTags";
 const LoginSecretComponent: FC = () => {
   const { secretSelected, currentModal } = useContext(DashboardContext);
-  const { loginInputsList, SSOToggle,TextArea } = useLogin();
+  const { loginInputsList, SSOToggle } = useLogin();
 
   if (currentModal !== "Login" && secretSelected !== "Login") return null;
 
