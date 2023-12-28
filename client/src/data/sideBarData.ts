d395ccdc9f8069608f19cb76f575eb36fdc5d039
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:26:43 -0500

    Refactor sidebar data structure

--- client/src/data/sideBarData.ts
+++ client/src/data/sideBarData.ts
@@ -1,56 +1,64 @@
 // Template for static sidebar data
 
 import { v4 } from "uuid";
-import type { IconNames } from "../types/reactTypes/iconNamesTypes";
 
-interface SideBarData {
-  id: string;
-  name: string;
-  icon: IconNames;
-  color?: string;
-  isCategory?: boolean;
-}
-const sideBarMain: SideBarData[] = [
-  { id: v4(), name: "All Secrets", icon: "AllSecrets", color: "#008080" },
-  { id: v4(), name: "Favorites", icon: "Favorite", color: "#f3f30dd2" },
+import { SideBarDataType } from "../types/reactTypes/SidebarDataType";
+
+
+const sideBarMain: SideBarDataType[] = [
+  {
+    id: v4(),
+    name: "All Secrets",
+    icon: "AllSecrets",
+    color: "#008080",
+  
+  },
+  {
+    id: v4(),
+    name: "Favorites",
+    icon: "Favorite",
+    color: "#f3f30dd2",
+  
+  },
+
   { id: v4(), name: "Deleted", icon: "Trash", color: "#bb2d38" },
 ];
 
-const sideBarSub: SideBarData[] = [
+const sideBarSub: SideBarDataType[] = [
   {
     id: v4(),
     name: "Logins",
     icon: "Login",
     color: "#FFA500",
-    isCategory: true,
+  
   },
   {
     id: v4(),
     name: "Payment Cards",
     icon: "Payment",
     color: "#D3D3D3",
-    isCategory: true,
+  
   },
   {
     id: v4(),
     name: "Drivers License",
     icon: "Id",
     color: "#a2c4c9",
-    isCategory: true,
+  
   },
   {
     id: v4(),
     name: "Documents",
     icon: "Document",
     color: "#e5cca5",
-    isCategory: true,
+  
   },
   {
     id: v4(),
     name: "Bank Accounts",
     icon: "Bank",
     color: "#79e29a",
-    isCategory: true,
+  
   },
 ];
 export { sideBarMain, sideBarSub };
