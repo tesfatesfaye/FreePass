9ba11a25ba2f07d801b1487d6345fb55aa9121f4
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-27 19:52:44 -0500

    Added TextArea component to LoginSecretComponent

--- client/src/components/secrets/selectedSecret/Login.tsx
+++ client/src/components/secrets/selectedSecret/Login.tsx
@@ -6,7 +6,7 @@ import TitleContainer from "../../../modals/UIcomponents/TitleContainer";
 import AddTags from "../../../modals/UIcomponents/AddTags";
 const LoginSecretComponent: FC = () => {
   const { secretSelected, currentModal } = useContext(DashboardContext);
-  const { loginInputsList, SSOToggle } = useLogin();
+  const { loginInputsList, SSOToggle,TextArea } = useLogin();
 
   if (currentModal !== "Login" && secretSelected !== "Login") return null;
 
@@ -16,18 +16,9 @@ const LoginSecretComponent: FC = () => {
         <TitleContainer type="Login" />
         <form className="flex flex-col gap-[1.5px]">
           {loginInputsList}
-          <div className=" bg-black bg-opacity-20 w-auto rounded-md flex flex-col pl-3 pb-4">
-            <label htmlFor="notes" className="text-purple-500 text-xs mt-1">
-              notes
-            </label>
-            <textarea
-              className=" text-slate-400 bg-black bg-opacity-0 text-xs h-8 flex items-center w-full outline-none  "
-              placeholder="Add any description of the secret here"
-              name="textarea"
-            />
-          </div>
+          <TextArea/>
           <SSOToggle />
-        <AddTags />
+          <AddTags />
         </form>
       </div>
     </Modal>
