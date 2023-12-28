cea58eb7dbfab4a10c8d0fe155dfa860918c3234
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:27:07 -0500

    Refactor ModalInput component to add label htmlFor attribute

--- client/src/modals/UIcomponents/ModalInput.tsx
+++ client/src/modals/UIcomponents/ModalInput.tsx
@@ -9,15 +9,18 @@ interface ModalInputProps {
 const ModalInput: FC <ModalInputProps> = ({ type, placeholder, name }) => {
   return (
     <div className=" bg-black bg-opacity-40  w-auto rounded-md flex flex-col pl-3 pb-2">
-      <label htmlFor="username" className="text-purple-500 text-xs mt-1">
+      <label htmlFor={name} className="text-purple-500 text-xs mt-1">
         {name}
       </label>
-      <input
-        className=" text-slate-400 bg-black bg-opacity-0  flex items-center w-full outline-none  "
-        placeholder={placeholder}
-        type={type}
-        name={name}
-      />
+      <label htmlFor={name} className="w-full">
+        <input
+          id={name}
+          className=" text-slate-400 bg-black bg-opacity-0  flex items-center w-full outline-none  "
+          placeholder={type !== "date" ? placeholder : ""}
+          type={type}
+          name={name}
+        />
+      </label>
     </div>
   );
 };
