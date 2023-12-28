191fde3012deafbcc4ec0b868357ac8c120dfded
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:26:38 -0500

    Add useEffect hook to SideBarItem component

--- client/src/components/sidebar/SideBarItem.tsx
+++ client/src/components/sidebar/SideBarItem.tsx
@@ -1,39 +1,32 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect } from "react";
 import { DashboardContext } from "../../context/DashboardContext";
-import type { SideBarItemType } from "../../types/reactTypes/sideBarType";
+import type { SideBarItemType } from "../../types/reactTypes/SideBarTypes";
 import Icon from "../icons/IconComponent";
-import type { SecretCategoryTypes } from "../../types/reactTypes/SecretCategoryTypes";
 
-const SideBarItem: FC<SideBarItemType> = (props) => {
-  const { sideBarItemIsSelected, toggleSideBarItemSelection,updateCurrentModal } =
+const SideBarItem: FC<SideBarItemType> = ({id, name,icon, color,isOpen=true}) => {
+  const { sideBarItemIsSelected, toggleSideBarItemSelection } =
     useContext(DashboardContext);
-    const toggleModal = (
-      e: React.MouseEvent<HTMLButtonElement>,
-      modalName: SecretCategoryTypes
-    ) => {
-      e.stopPropagation();
-      updateCurrentModal(modalName);
-    };
+    useEffect(() => {
+      console.log(isOpen)
+    },[isOpen])
   return (
     <button
-      key={props.id}
-      onClick={() => toggleSideBarItemSelection(props.name)}
+      key={id}
+      onClick={() => toggleSideBarItemSelection(name)}
       className={`flex text-white items-center gap-2  pl-2 pt-0 pb-0 cursor-pointer 
           pr-auto overflow-hidden transition-all duration-200 text-lg box-border
            ${
-             props.isOpen
+            isOpen === true 
                ? " mb-2 max-h-[3rem] pt-[0.05rem] pb-[0.05rem]"
                : "m-0 p-0  max-h-0 "
            } ${
-        props.name === sideBarItemIsSelected
+       name === sideBarItemIsSelected
           ? " bg-white bg-opacity-20"
           : " hover:bg-white hover:bg-opacity-10"
       } `}
     >
-      <Icon type={props.icon} color={props.color} />
-      {props.name}
-      {props.isCategory && <Icon type="Add" size={20} className="ml-auto mr-4" onClick={(e)=>{toggleModal(e,props.icon)}} />}
-     
+      <Icon type={icon} color={color} />
+      {name}
     </button>
   );
 };
