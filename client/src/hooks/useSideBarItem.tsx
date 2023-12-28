a4162b70b99cf9f859db115748eaa4b161965423
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 18:02:51 -0500

    Refactor useSidebarItem hook to accept SecretCategoryTypes

--- client/src/hooks/useSideBarItem.tsx
+++ client/src/hooks/useSideBarItem.tsx
@@ -1,14 +1,18 @@
 import {useContext,MouseEvent} from "react";
 import { DashboardContext } from "../context/DashboardContext";
+import type { SecretCategoryTypes } from "../types/reactTypes/SecretCategoryTypes";
 
 const useSidebarItem=()=>{
     
     const{updateCurrentModal}=useContext(DashboardContext)
 
-    const toggle=(e:MouseEvent<SVGSVGElement>,modal:string|undefined)=>{
-        e.stopPropagation()
-        updateCurrentModal(modal)
-    }
+    const toggle = (
+      e: MouseEvent<SVGSVGElement>,
+      modal: SecretCategoryTypes
+    ) => {
+      e.stopPropagation();
+      updateCurrentModal(modal);
+    };
     return {toggle}
 }
 export default useSidebarItem
