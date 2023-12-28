fd16e354aebd8234e116d10e0e9f407b486aeb00
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 18:02:45 -0500

    Fix typo in usePayment.tsx

--- client/src/hooks/usePayment.tsx
+++ client/src/hooks/usePayment.tsx
@@ -15,7 +15,7 @@ const usePayment = () => {
     { id: v4(), type: "text", placeholder: "CVV", name: "cvv" },
     { id: v4(), type: "text", placeholder: "PIN", name: "pin" },
     { id: v4(), type: "date", placeholder: "", name: "validFrom" },
-    { id: v4(), type: "Date", placeholder: "", name: "expiryDate" },
+    { id: v4(), type: "date", placeholder: "", name: "expiryDate" },
   ]);
 
   const paymentInputsList = paymentInputs.map((input) => {
