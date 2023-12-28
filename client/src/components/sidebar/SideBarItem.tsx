4ecf044ce5035f08ad824b1c80cb36007f322099
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-27 20:19:00 -0500

    Refactor SideBarItem component to use updated IconComponent

--- client/src/components/sidebar/SideBarItem.tsx
+++ client/src/components/sidebar/SideBarItem.tsx
@@ -1,11 +1,19 @@
 import { FC, useContext } from "react";
 import { DashboardContext } from "../../context/DashboardContext";
 import type { SideBarItemType } from "../../types/reactTypes/sideBarType";
-import IconComponent from "../icons/IconComponent";
+import Icon from "../icons/IconComponent";
+import type { SecretCategoryTypes } from "../../types/reactTypes/SecretCategoryTypes";
 
 const SideBarItem: FC<SideBarItemType> = (props) => {
-  const { sideBarItemIsSelected, toggleSideBarItemSelection } =
+  const { sideBarItemIsSelected, toggleSideBarItemSelection,updateCurrentModal } =
     useContext(DashboardContext);
+    const toggleModal = (
+      e: React.MouseEvent<HTMLButtonElement>,
+      modalName: SecretCategoryTypes
+    ) => {
+      e.stopPropagation();
+      updateCurrentModal(modalName);
+    };
   return (
     <button
       key={props.id}
@@ -22,8 +30,10 @@ const SideBarItem: FC<SideBarItemType> = (props) => {
           : " hover:bg-white hover:bg-opacity-10"
       } `}
     >
-      <IconComponent type={props.icon} color={props.color} />
+      <Icon type={props.icon} color={props.color} />
       {props.name}
+      {props.isCategory && <Icon type="Add" size={20} className="ml-auto mr-4" onClick={(e)=>{toggleModal(e,props.icon)}} />}
+     
     </button>
   );
 };
