be13df3f2c0f2a51b8588127ad59a99ed70d74cf
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 00:27:03 -0500

    Refactor useSidebar hook and update SideBarItem imports

--- client/src/hooks/useSidebar.tsx
+++ client/src/hooks/useSidebar.tsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
-import type { SideBarItemType } from "../types/reactTypes/sideBarType";
 import { sideBarMain, sideBarSub } from "../data/sideBarData";
+import type { SideBarItemType } from "../types/reactTypes/SideBarTypes";
+import SideBarItem from "../components/sidebar/SideBarItem";
 
 const useSidebar = () => {
-  const [sidebarMainItems, setSidebarMainItems] =
+  const [isCategoriesDropdownOpen, setIsCategoriesDropdownOpen] =
+    useState<boolean>(true);
+  const [sidebarMainItems] =
     useState<SideBarItemType[]>(sideBarMain);
-    const [sidebarSubItems, setSidebarSubItems] =
-    useState<SideBarItemType[]>(sideBarSub);
-    const [isCategoriesDropdownOpen, setIsCategoriesDropdownOpen] =
-      useState<boolean>(true);
-    const [isTagsDropdownOpen, setIsTagsDropdownOpen] =useState<boolean>(true);
-    const toggleCategoriesDropdown = () => {
-      setIsCategoriesDropdownOpen(prev=>!prev);
-    };
-    const toggleTagsDropdown = () => {
-      setIsTagsDropdownOpen(prev=>!prev);
-    };
-
+  const [sidebarSubItems] = useState<SideBarItemType[]>(
+    () =>
+      sideBarSub.map((item) => ({
+        ...item,
+        isOpen: isCategoriesDropdownOpen,
+      }))
+  );
+    
+  const [isTagsDropdownOpen, setIsTagsDropdownOpen] = useState<boolean>(true);
+  const toggleCategoriesDropdown = () => {
+    setIsCategoriesDropdownOpen((prev) => !prev);
+  };
+  const toggleTagsDropdown = () => {
+    setIsTagsDropdownOpen((prev) => !prev)
+        };
+      
+      const SidebarSubMapped = sidebarSubItems.map((item) => (
+        <SideBarItem
+          key={item.id}
+          {...item}
+          isOpen={isCategoriesDropdownOpen}
+        />
+      ));
   return {
     sidebarMainItems,
-    sidebarSubItems,
     isCategoriesDropdownOpen,
     isTagsDropdownOpen,
     toggleCategoriesDropdown,
     toggleTagsDropdown,
-    
-    
+    SidebarSubMapped,
   };
 };
 export default useSidebar;
