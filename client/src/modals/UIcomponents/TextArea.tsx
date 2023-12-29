a8c528d1f82a93e436982b809df1ea37548b41b7
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 23:50:12 -0500

    Refactor TextArea component styles

--- client/src/modals/UIcomponents/TextArea.tsx
+++ client/src/modals/UIcomponents/TextArea.tsx
@@ -10,12 +10,13 @@ const ModalInput: FC<ModalInputProps> = ({
   name = "Notes",
 }) => {
   return (
-    <div className=" bg-black bg-opacity-20 w-auto rounded-md flex flex-col pl-3 pb-4">
+    <div className="bg-black bg-opacity-20 w-auto rounded-md flex flex-col pl-3 pb-4">
       <label htmlFor="notes" className="text-purple-500 text-xs mt-1">
         notes
       </label>
+
       <AutoSize
-        className=" text-slate-400 bg-transparent text-sm h-8 flex items-center w-full outline-none  "
+        className="text-slate-400 bg-transparent text-sm h-8 flex items-center w-full outline-none overflow-y-visible resize-none"
         placeholder={placeholder}
         name={name}
         minRows={2}
