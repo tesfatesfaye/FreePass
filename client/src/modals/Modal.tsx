046e52901ada03a206695cb69b9c5945296d7d36
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 23:50:09 -0500

    Add ScrollWrapper and update Modal styling

--- client/src/modals/Modal.tsx
+++ client/src/modals/Modal.tsx
@@ -1,7 +1,8 @@
 import { FC, ReactNode, useContext } from "react";
+import ScrollWrapper from "../components/ScrollBarWrapper";
+import Icon from "../components/icons/IconComponent";
 import { DashboardContext } from "../context/DashboardContext";
 import ReactPortal from "./ReactPortal";
-import Icon from "../components/icons/IconComponent";
 interface ModalProps {
   children: ReactNode;
 }
@@ -11,13 +12,18 @@ const Modal: FC<ModalProps> = ({ children }) => {
   return (
     <ReactPortal wrapperId="portal">
       <div className="modal-overlay"></div>
+
       <div className="modal-container">
         <div
-          className="flex flex-col relative
+          className="flex flex-col relative overflow-y-hidden;
         h-[95%] bg-p-dark rounded-md shadow-2xl w-[30%] max-w-[700px] "
         >
           <header className="flex items-center h-[4rem] justify-between px-4 rounded-t-md bg-s-dark">
-            <Icon type={currentModal} size={45}className="text-white text-lg  ml-auto mr-auto"/>
+            <Icon
+              type={currentModal}
+              size={45}
+              className="text-white text-lg  ml-auto mr-auto"
+            />
             <button onClick={() => updateCurrentModal(null)}>
               <Icon
                 className="text-white flex items-center text-lg font-bold mr-2"
@@ -26,12 +32,14 @@ const Modal: FC<ModalProps> = ({ children }) => {
               />
             </button>
           </header>
-           <div className="secret-form-parent ">
-             {children}
-            </div>
-         
-          <footer className="flex items-center h-[3rem] mt-auto justify-between px-4 
-          rounded-b-md bg-s-dark">
+          <div className="secret-form-parent mb-3">
+            <ScrollWrapper>{children}</ScrollWrapper>
+          </div>
+
+          <footer
+            className="flex items-center h-[3rem] min-h-[3rem] mt-auto justify-between px-4 
+          rounded-b-md bg-s-dark"
+          >
             <button
               className="text-white 
                flex w-[5rem] items-center justify-center h-[30px]
