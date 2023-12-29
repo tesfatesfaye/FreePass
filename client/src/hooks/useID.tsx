4c31697cd319d2e1832a9ac2a9f346406e05568f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-28 23:08:16 -0500

    Add useID hook for ID inputs

---
+++ client/src/hooks/useID.tsx
@@ -0,0 +1,21 @@
+import {  useState } from "react";
+import ModalInput from "../modals/UIcomponents/ModalInput";
+import { idCardModalData } from "../data/secretsModalData";
+const useID = () => {
+ const [idInputs] = useState(idCardModalData);
+
+  const idInputList = idInputs.map((input) => {
+    return (
+      <ModalInput
+        key={input.id}
+        type={input.type}
+        placeholder={input.placeholder}
+        name={input.name}
+      />
+    );
+  });
+
+  return { idInputList };
+};
+
+export default useID;
