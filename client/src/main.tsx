21282cd3fbad13fe4d30fe0995d525fe25b50cb2
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-31 16:04:08 -0500

    Add Global CSS import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./assets/Global.css";
 // import AuthProvider from "./context/AuthProvider.tsx";
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   // <AuthProvider>
